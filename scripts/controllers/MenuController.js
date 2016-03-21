fc4fd7f57ec2f9c825a4f2ec6c8d2fcae818f0be
Lautaro German <lautaro.zuniga@babel.es>
2016-03-21 11:17:23 +0100

    Ejemplo sencillo de selección de menú con angularJS

---
+++ scripts/controllers/MenuController.js
@@ -0,0 +1,19 @@
+//En el módulo moviedb, defino el controlador
+angular.module("moviedb")
+    .controller("MenuController", ["$scope", function ($scope) {
+    //scope init
+    $scope.model = {
+        selectedItem: "movies"
+    };
+    //Scope methods
+    $scope.setSelectedItem = function(item){
+        $scope.model.selectedItem = item;
+    }
+    $scope.getClassForItem = function(item){
+        if($scope.model.selectedItem == item){
+            return "active";
+        }else{
+            return "";
+        }
+    }
+}]);
