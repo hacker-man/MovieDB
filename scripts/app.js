3e4c41f4d7c4e67790390f28d4e68d0509d6c776
Lautaro German <lautaro.zuniga@babel.es>
2016-03-21 13:59:02 +0100

    Actulizando url con angular.

--- scripts/app.js
+++ scripts/app.js
@@ -1,2 +1,14 @@
 //Defino el módulo "moviedb"
-angular.module("moviedb",[]);
+angular.module("moviedb", ['ngRoute'])
+    .config(["$routeProvider", function ($routeProvider) {
+        $routeProvider.when('/movies', {
+            templateUrl: 'views/SeriesList.html'
+        }).when('/series', {
+            templateUrl: 'views/PeopleList.html'
+        }).when('/',{
+            redirectTo: '/movies'
+        }).otherwise({
+            templateUrl: 'views/404.html'
+        })
+    }]
+);
