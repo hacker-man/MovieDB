114fdc44f87b0540e309327b48a3c7abddcddf66
Lautaro Germán <laut.german@gmail.com>
2016-03-28 20:55:15 +0200

    Intento arregalar filtro categoría.

--- scripts/filters/join.js
+++ scripts/filters/join.js
@@ -1,15 +1,13 @@
-angular.module("moviedb").filter("join",
-  ["$log",function($log){
-      return function(arr,sep){
-          var items = arr || null;
-          if(items=null){
-              return "";
-          }
-          if(typeof arr.join === "undefined"){
-              $log.error("The value passed to the filter 'join' must be an array");
-              return "";
-          }
-          return arr.join(separator);
-      };
-  }]
-);
+angular.module("moviedb").filter("join", ["$log", function ($log) {
+	return function (arr, sep) {
+		var items = arr || null;
+		var separator = sep || ", ";
+		if (items == null)
+			return "";
+		if (typeof arr.join === "undefined") {
+			$log.error("The value passed to the filter 'join' must be an array");
+			return "";
+		}
+		return arr.join(separator);
+	};
+  }]);
